a6a2c90621f9a7e3fcbcd8970b15cdb51016a358
Neha <nehaboora49@gmail.com>
2025-07-28 16:14:07 +0530

    created delicious section

--- src/components/Delicious.jsx
+++ src/components/Delicious.jsx
@@ -1,8 +1,28 @@
 import React from 'react'
+import backgroundImage1 from '../assets/webp/deliciousimg1.webp'
+import backgroundImage2 from '../assets/webp/deliciousimg2.webp'
+import Description from './common/Description'
+import SubHeading from './common/SubHeading'
+import Button from './common/Button'
 
 const Delicious = () => {
   return (
-    <div>Delicious</div>
+    <div className='max-w-[1164px] px-3 mx-auto'>
+      <div className='flex gap-6 '>
+        <div className='w-1/2 bg-center bg-no-repeat bg-cover rounded-xl pl-8 py-[63px]' style={{ backgroundImage: `url(${backgroundImage1})` }}>
+            <Description className={'text-prime !text-[28px]'} text={'Delicious'}/>
+            <SubHeading className={'text-white'} text={'Spicy Paneer'} />
+            <Description className={'text-white text-[28px] mb-4'} text={'Limited Time'} />
+            <Button className={'!py-[10px] !px-4'}>Order Now </Button>
+        </div>
+        <div className='w-1/2 bg-center bg-no-repeat bg-cover rounded-xl pl-8 py-[63px]' style={{ backgroundImage: `url(${backgroundImage2})` }}>
+            <Description className={'text-prime !text-[28px]'} text={'Delicious'}/>
+            <SubHeading className={'text-white'} text={'CHEESE BURST'} />
+            <Description className={'text-white text-[28px] mb-4'} text={'Limited Time'} />
+            <Button className={'!py-[10px] !px-4'}>Order Now </Button>
+        </div>
+      </div>
+    </div>
   )
 }
 
--- src/utils/icon.jsx
+++ src/utils/icon.jsx
@@ -5,9 +5,9 @@ export const LeftLine = () => (
         <path d="M60.5 1H0.491669" stroke="url(#paint0_linear_133_5506)" />
         <defs>
             <linearGradient id="paint0_linear_133_5506" x1="60.5" y1="1.76761" x2="56.4905" y2="-15.3725" gradientUnits="userSpaceOnUse">
-                <stop stop-color="#EC6112" />
-                <stop offset="0.380908" stop-color="#FF902E" />
-                <stop offset="0.74047" stop-color="#FEFEFE" stop-opacity="0" />
+                <stop stopColor="#EC6112" />
+                <stop offset="0.380908" stopColor="#FF902E" />
+                <stop offset="0.74047" stopColor="#FEFEFE" stopOpacity="0" />
             </linearGradient>
         </defs>
     </svg>
@@ -21,9 +21,9 @@ export const RightLine = () => (
         <path d="M0.5 1H60.5083" stroke="url(#paint0_linear_133_5508)" />
         <defs>
             <linearGradient id="paint0_linear_133_5508" x1="0.5" y1="1.76761" x2="4.5095" y2="-15.3725" gradientUnits="userSpaceOnUse">
-                <stop stop-color="#EC6112" />
-                <stop offset="0.380908" stop-color="#FF902E" />
-                <stop offset="0.74047" stop-color="#FEFEFE" stop-opacity="0" />
+                <stop stopColor="#EC6112" />
+                <stop offset="0.380908" stopColor="#FF902E" />
+                <stop offset="0.74047" stopColor="#FEFEFE" stopOpacity="0" />
             </linearGradient>
         </defs>
     </svg>
