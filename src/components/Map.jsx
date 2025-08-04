115fb68fb664ac532e9f2c30b0b611616adc3bcf
Neha <nehaboora49@gmail.com>
2025-08-04 10:52:00 +0530

    created map section

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -2,7 +2,10 @@ import React from 'react'
 
 const Map = () => {
   return (
-    <div>Map</div>
+    <div>
+      <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d223017.5019665298!2d76.82611507737823!3d28.422693756401628!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x390d19d582e38859%3A0x2cf5fe8e5c64b1e!2sGurugram%2C%20Haryana!5e0!3m2!1sen!2sin!4v1754284578615!5m2!1sen!2sin" width="100%" height="450"  allowFullScreen="" loading="lazy" referrerPolicy="no-referrer-when-downgrade">
+      </iframe>
+    </div>
   )
 }
 
