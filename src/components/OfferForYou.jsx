5289019336500785e28905d506c6890c79472831
Neha <nehaboora49@gmail.com>
2025-08-05 09:30:42 +0530

    created the offer for you

--- src/components/OfferForYou.jsx
+++ src/components/OfferForYou.jsx
@@ -12,12 +12,12 @@ const OfferForYou = () => {
       <div className='max-w-[1140px]'>
         <SubHeading className={'!text-[32px] mb-[24px]'} text={'Offers for You'} />
         <div className='flex gap-[24px]'>
-          <div className=' relative overflow-hidden bg-cover bg-center bg-no-repeat rounded-lg w-1/2 h-[300px]' style={{ backgroundImage: `url(${backgroundImage1})` }}>
-            heloo
+          <div className=' pl-[24px] flex items-center relative overflow-hidden bg-cover bg-center bg-no-repeat rounded-lg w-1/2 h-[300px]' style={{ backgroundImage: `url(${backgroundImage1})` }}>
+            <SubHeading className={'max-w-[316px] w-full'} text={'30% Off on Order Above ₹ 249'} />
             <img src={pizza1} alt="img" className='absolute right-[-130px] top-[-25px]' />
           </div>
-          <div className=' relative overflow-hidden bg-cover bg-center bg-no-repeat rounded-l w-1/2 h-[300px]' style={{ backgroundImage: `url(${backgroundImage2})` }}>
-            jjk
+          <div className='pl-[24px] flex items-center relative overflow-hidden bg-cover bg-center bg-no-repeat rounded-l w-1/2 h-[300px]' style={{ backgroundImage: `url(${backgroundImage2})` }}>
+            <SubHeading className={'text-white max-w-[296px] w-full'} text={'₹ 50 Cashback on First Order'} />
             <img src={pizza2} alt="img" className='absolute right-[-175px] top-[-22px]' />
           </div>
         </div>
